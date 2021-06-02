208725d1acdf64903574fa2692296cc20c173992
William Kirouac-Samson <wks@Williams-MacBook-Pro.local>
2021-06-02 00:31:37 -0400

    baniere a perfectionner

--- src/components/hero-slider/HeroSliderStatic.js
+++ src/components/hero-slider/HeroSliderStatic.js
@@ -2,22 +2,20 @@ import PropTypes from "prop-types";
 import React from "react";
 const HeroSliderStatic = ({ string, pitch1, pitch2, pitch3, sliderText, sliderImage }) => {
   return (
-
-
+<div className="acceuil">
 <div className="row">
-      <div className="col-md-4 ml-auto order-md-2 align-self-start">
-        <div className="site-block-cover-content">
-            
-            <h2 className="sub-title">{pitch1}</h2>
-            <h1>{pitch2}</h1>
-            <p><a href="#" className="btn btn-black rounded-0">{pitch3}</a></p>
-        </div>
+      <div className="col-md-8 col-sm-12">
+          <img src="assets/img/slider/F45-Website-1.jpg" alt="Image" className="img-fluid"/>
       </div>
-      <div className="col-md-8 order-1 align-self-end">
-          <img src="assets/img/slider/modified-png.png" alt="Image" className="img-fluid"/>
+      <div className="col-md-4 col-sm-12">
+      <h2>Perfecto poele et foyer</h2>
+      <h4>Ventes et installations de poeles et foyers</h4>
       </div>
+    </div>
 </div>
-      
+
+
+    
 
   );
 };
