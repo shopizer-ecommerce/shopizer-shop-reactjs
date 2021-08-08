6a67099c1fcc1bc3942e422a2152a2dc1ae6faa2
shopizer-ecommerce <csamson777@yahoo.com>
2021-08-08 19:25:14 -0400

    back url

--- env-config.js
+++ env-config.js
@@ -1,6 +1,6 @@
 window._env_ = {
   APP_PRODUCTION: "false",
-  APP_BASE_URL: "https://api.boutiquerufina.com",
+  APP_BASE_URL: "https://api.perfectopoelesetfoyers.com",
   APP_API_VERSION: "/api/v1/",
   APP_MERCHANT: "DEFAULT",
   APP_PRODUCT_GRID_LIMIT: "15",
@@ -9,6 +9,5 @@ window._env_ = {
   APP_NUVEI_SECRET: "",
   APP_PAYMENT_TYPE: "STRIPE",
   APP_STRIPE_KEY: "pk_test_TYooMQauvdEDq54NiTphI7jx,",
-  sAPP_THEME_COLOR: "#7c515d",
-  sAPP_THEME_COLOR: "#7c515d",
+  APP_THEME_COLOR: "#7c515d"
 }
