e68fba1b07fb90f745ddaaa84a87d28d75b2caeb
shopizer-ecommerce <csamson777@yahoo.com>
2021-08-10 23:30:21 -0400

    revert

--- public/env-config.js
+++ public/env-config.js
@@ -9,6 +9,7 @@ window._env_ = {
   APP_NUVEI_SECRET: "",
   APP_PAYMENT_TYPE: "STRIPE",
   APP_STRIPE_KEY: "pk_test_TYooMQauvdEDq54NiTphI7jx,",
-  sAPP_THEME_COLOR: "#7c515d",
-  sAPP_THEME_COLOR: "#7c515d",
+  APP_THEME_COLOR: "#7c515d,",
+  APP_THEME_COLOR: "#7c515d,",
+  APP_THEME_COLOR: "#7c515d,",
 }
