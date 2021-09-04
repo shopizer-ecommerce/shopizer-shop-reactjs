983ec524abd281a1dcaaf79619abcda57b730b10
shopizer-ecommerce <csamson777@yahoo.com>
2021-09-04 09:21:05 -0400

    Mobile menu

--- src/components/header/MobileMenu.js
+++ src/components/header/MobileMenu.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 // import MobileMenuSearch from "./sub-components/MobileSearch";
 import MobileNavMenu from "./sub-components/MobileNavMenu";
-import MobileLangCurChange from "./sub-components/MobileLangCurrChange";
 import MobileWidgets from "./sub-components/MobileWidgets";
 
 const MobileMenu = ({ categories, contents }) => {
