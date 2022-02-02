e4e1ab0765a4541dfb6269298abdb69b77d5f3ce
shopizerecomm <csamson777@yahoo.com>
2022-02-01 20:03:41 -0500

    Missing env file

---
+++ public/env-config.js
@@ -0,0 +1,13 @@
+window._env_ = {
+  APP_PRODUCTION: "false",
+  APP_BASE_URL: "http://localhost:8080",
+  APP_API_VERSION: "/api/v1/",
+  APP_MERCHANT: "DEFAULT",
+  APP_PRODUCT_GRID_LIMIT: "15",
+  APP_MAP_API_KEY: "",
+  APP_NUVEI_TERMINAL_ID: "",
+  APP_NUVEI_SECRET: "",
+  APP_PAYMENT_TYPE: "STRIPE",
+  APP_STRIPE_KEY: "pk_test_TYooMQauvdEDq54NiTphI7jx",
+  APP_THEME_COLOR: "#D1D1D1",
+}
